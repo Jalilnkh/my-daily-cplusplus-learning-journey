accb6ba5d9eb01687010f63ad784550451b1c45a
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-10-24 20:36:30 +0200

    sum up pointer

--- Week 2: Basic Structures: Vectors, Pointers, Strings, and Files/pointer.cpp
+++ Week 2: Basic Structures: Vectors, Pointers, Strings, and Files/pointer.cpp
@@ -6,8 +6,11 @@ pointers point to the memory address of the data that they are associated with.
 Pointer Reference: 
 A memory address is denoted with the & symbol, called the reference operator, 
 and they go in front of the variable that the address is associated with.
+Pointer Dereference:
+Every memory address holds a value and that value can be accessed by using the dereference operator. 
 1- A pointer
 2- Pointer Reference
+3- Pointer Dereference
 */
 
 #include <iostream>
@@ -27,6 +30,13 @@ int main() {
     int* p2 = &a;
 
     cout << p2 << endl;
+    cout << "3- Pointer Dereference" << endl;
+    int a3 = 5;
+    int* p3 = &a3;
+    int** p31 = &p3;
+
+    cout << *p3 << endl;
+    cout << **p31 << endl;
     //add code above this line
   
   return 0;
