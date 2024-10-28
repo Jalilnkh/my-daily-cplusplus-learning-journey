964cbbc391eb5f47fe46593724f78f94788952d9
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-10-28 22:40:51 +0100

    exercise a bit

---
+++ Week 2: Basic Structures: Vectors, Pointers, Strings, and Files/exercises_1.cpp
@@ -0,0 +1,37 @@
+/**
+
+Strings Exercise 1
+Problem
+Write a program that takes input from a user and prints the first character of the input 
+and the last character with some context. For example, if baseball is the string, 
+then the program will output b is the first character and l is the last character. 
+If cat is the string, then c is the first character and t is the last character will be printed.
+
+Strings Exercise 2
+Problem
+Write a program that captures input from the user, prints the input string as many times as its length, 
+and prints as many lines of it as the length of the input string.
+
+Exercise 3
+Problem
+Write a program that takes input without white spaces from a user, prints the first half of the string on one line, 
+and the second half on another. In the case of a string that has an odd number of characters, 
+the second line will have the extra character.
+
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  string my_string = (argv[1]);
+  
+  //add code below this line
+
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
