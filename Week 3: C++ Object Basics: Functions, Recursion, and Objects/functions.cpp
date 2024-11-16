06b9242bd54ac95159e1eb47914f30130ca86699
Jalil Nourmohammadi Khiarak <jalil.nourmohammadi89@gmail.com>
2024-11-16 13:41:52 +0100

    Code Documentation

--- Week 3: C++ Object Basics: Functions, Recursion, and Objects/functions.cpp
+++ Week 3: C++ Object Basics: Functions, Recursion, and Objects/functions.cpp
@@ -3,7 +3,9 @@ Functions are composed of two parts, the header and the body.
 The function header may contain several keywords to determine the function type. Next is the name of the function.
 The function body is the list of actions the function performs. 
 All of the code for the function body should be enclosed within curly braces {} and indented as a best practice to show association. 
-
+C++ Code Documentation
+Including C++ code documentation prior to function definitions is standard. 
+Doing so enables users to gain clarity on the purpose of the function, any parameters that are used, and what the function returns.
 1- Calling a Function
 2- C++ Code Documentation
 */
@@ -20,9 +22,15 @@ int main() {
     // 1- Calling a Function
     cout << "1- Calling a Function" << endl;
     GreetTwice()
-    // 2- Array Modification  
-    cout << "2- Array Modification" << endl;
-    
+    // 2- C++ Code Documentation 
+    cout << "2- C++ Code Documentation" << endl;
+    /**
+     * This function greets the user twice
+     * 
+     * @param   specify parameters if any
+     * @return  specify return value if any
+    */
+    GreetTwice()
     // 3- 2D Array  
     cout << "3- 2D Array" << endl;
     
