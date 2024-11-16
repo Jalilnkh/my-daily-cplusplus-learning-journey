c74e55f86fa5ee7b1a99961d8b5efe09eebdb345
Jalil Nourmohammadi Khiarak <jalil.nourmohammadi89@gmail.com>
2024-11-16 13:30:48 +0100

    Create functions.cpp

---
+++ Week 3: C++ Object Basics: Functions, Recursion, and Objects/functions.cpp
@@ -0,0 +1,7 @@
+/* 
+Functions are composed of two parts, the header and the body.
+The function header may contain several keywords to determine the function type. Next is the name of the function.
+The function body is the list of actions the function performs. 
+All of the code for the function body should be enclosed within curly braces {} and indented as a best practice to show association. 
+
+*/
