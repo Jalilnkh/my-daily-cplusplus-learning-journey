f19eca311989cf4b22f21db32743bbb1c26593b6
Jalil Nourmohammadi Khiarak <jalil.nourmohammadi89@gmail.com>
2024-11-16 13:39:39 +0100

    Update functions.cpp

--- Week 3: C++ Object Basics: Functions, Recursion, and Objects/functions.cpp
+++ Week 3: C++ Object Basics: Functions, Recursion, and Objects/functions.cpp
@@ -4,4 +4,30 @@ The function header may contain several keywords to determine the function type.
 The function body is the list of actions the function performs. 
 All of the code for the function body should be enclosed within curly braces {} and indented as a best practice to show association. 
 
+1- Calling a Function
+2- C++ Code Documentation
 */
+#include <iostream>
+using namespace std;
+void GreetTwice() {
+  cout << "Goodbye" << endl;
+  cout << "Hello" << endl;
+  cout << "Hello" << endl;
+}
+int main() {
+  
+    //add code below this line
+    // 1- Calling a Function
+    cout << "1- Calling a Function" << endl;
+    GreetTwice()
+    // 2- Array Modification  
+    cout << "2- Array Modification" << endl;
+    
+    // 3- 2D Array  
+    cout << "3- 2D Array" << endl;
+    
+  //add code above this line
+  
+  return 0;
+  
+}
