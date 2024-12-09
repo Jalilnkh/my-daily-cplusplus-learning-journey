e9bfc119c1cf5be194f64a93c418032bfae6aec3
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-12-09 23:26:41 +0100

    work on setter

--- Week 4: Object-Oriented C++: Inheritance and Encapsulation/getter_setter.cpp
+++ Week 4: Object-Oriented C++: Inheritance and Encapsulation/getter_setter.cpp
@@ -3,8 +3,11 @@ What is Getters?
 While all attributes are private, that does not mean that the user will not need to access the values of these attributes. 
 A public function can be used to access a private attribute. 
 This type of function has a special name, a getter (also called an accessor). 
+Setters
+Setters are the compliment to getters in that they allow you to set the value of a private attribute. 
+Setter functions are also called mutators. 
 1- Getters
-2- 
+2- Setters
 3- 
 */
 #include <iostream>
@@ -21,6 +24,10 @@ class Phone {
     string GetModel() {
       return model;
     }
+
+    void SetModel(string new_model) {
+      model = new_model;
+    }
   
   private:
     string model;
@@ -36,9 +43,10 @@ int main() {
     cout << "1- Getters" << endl;
     Phone model("iphone", 2, 3);
     cout << model.GetModel()<< endl;
-    // 2- Public and Private Function
-    cout << "2- Public and Private Function" << endl;
-    // Now if we call private we will get error:
+    // 2- Setters
+    cout << "2- Setters" << endl;
+    model.SetModel("Idone");
+    cout << model.GetModel() << endl;
     // 3- The “const” Keyword
     cout << "3- The “const” Keyword" << endl;
 
