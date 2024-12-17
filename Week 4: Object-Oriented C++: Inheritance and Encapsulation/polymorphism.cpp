c713cf9ec743205aa0f4d145c6c728aef1d2faa7
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-12-17 22:44:23 +0100

    work on polymorphism

---
+++ Week 4: Object-Oriented C++: Inheritance and Encapsulation/polymorphism.cpp
@@ -0,0 +1,30 @@
+/* 
+What is Polymorphism?
+Polymorphism is a concept in object-oriented programming in which a single interface takes different forms 
+(polymorphism means “many forms”). Often this means similar operations are grouped together with the same name.
+1- Polymorphism
+2- 
+3- 
+*/
+#include <iostream>
+using namespace std;
+    
+
+  
+
+int main() {
+  
+    //add code below this line
+    // 1- Polymorphism
+    cout << "1- Polymorphism" << endl;
+
+    // 2- 
+    cout << "2- " << endl;
+    // 3- 
+    cout << "3- " << endl;
+
+    //add code above this line
+
+  return 0;
+  
+}
